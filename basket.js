dce289c77843a4c834e490a4295bf94a21c891e2
sniperdev <62942349+sniperdev@users.noreply.github.com>
2022-11-09 22:31:29 +0100

    <feat> product and basket class. dummy data

--- basket.js
+++ basket.js
@@ -1,1 +1,17 @@
-class Basket {}
+class Basket {
+  constructor() {
+    this.products = [];
+  }
+
+  add(product) {
+    this.products.push(product);
+  }
+}
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+const basket = new Basket();
